4c635569992bfc0fbf102714462676576d1e3efd
Locxion <locxion@locxserv.de>
2016-01-27 22:32:33 +0100

    Medical Items & Radio added

--- Altis_Life.Altis/Config_Weapons.hpp
+++ Altis_Life.Altis/Config_Weapons.hpp
@@ -18,26 +18,27 @@ class WeaponShops {
         license = "gun";
         level[] = { "", "", -1, "" };
         items[] = {
-         //Handguns
-         { "hgun_P07_F", "", 7650},
-         { "hgun_Rook40_F", "", 7650 },
-         { "16Rnd_9x21_Mag", "", 100},
-         { "30Rnd_9x21_Mag", "", 150},
-         { "hgun_ACPC2_F", "", 11500 },
-         { "9Rnd_45ACP_Mag", "", 100},
-         { "hgun_Pistol_heavy_02_F", "", 11500 },
-         { "6Rnd_45ACP_Cylinder", "", 50},
-         { "hgun_Pistol_heavy_01_F", "", 15300},
-         { "11Rnd_45ACP_Mag", "", 150}
+        //Handguns
+        { "hgun_P07_F", "", 7650},
+        { "hgun_Rook40_F", "", 7650 },
+        { "16Rnd_9x21_Mag", "", 100},
+        { "30Rnd_9x21_Mag", "", 150},
+        { "hgun_ACPC2_F", "", 11500 },
+        { "9Rnd_45ACP_Mag", "", 100},
+        { "hgun_Pistol_heavy_02_F", "", 11500 },
+        { "6Rnd_45ACP_Cylinder", "", 50},
+        { "hgun_Pistol_heavy_01_F", "", 15300},
+        { "11Rnd_45ACP_Mag", "", 150},
+		{ "cse_bandage_basic", "Einfache Bandage", 50, 25 }
          
-         //SMG's
-         //SMG{ "hgun_PDW2000_F", "", 20000 },
-         //SMG{ "16Rnd_9x21_Mag", "", 100},
-         //SMG{ "30Rnd_9x21_Mag", "", 150},
-         //SMG{"SMG_02_F", "", 19100},
-         //SMG{ "30Rnd_9x21_Mag", "", 150},
-         //SMG{"30Rnd_45ACP_MAG_SMG_01", "", 200},
-         //SMG{"30Rnd_45ACP_Mag_SMG_01_tracer_green", "", 150}
+        //SMG's
+        //SMG{ "hgun_PDW2000_F", "", 20000 },
+        //SMG{ "16Rnd_9x21_Mag", "", 100},
+        //SMG{ "30Rnd_9x21_Mag", "", 150},
+        //SMG{"SMG_02_F", "", 19100},
+        //SMG{ "30Rnd_9x21_Mag", "", 150},
+        //SMG{"30Rnd_45ACP_MAG_SMG_01", "", 200},
+        //SMG{"30Rnd_45ACP_Mag_SMG_01_tracer_green", "", 150}
 		};
 	};
 
@@ -47,76 +48,76 @@ class WeaponShops {
         license = "rebel";
         level[] = { "", "", -1, "" };
         items[] = {
-         //SMG's
-         { "hgun_PDW2000_F", "", 20000 },//Wenn Legal, im CIV shop anbieten!
-         { "16Rnd_9x21_Mag", "", 100},
-         { "SMG_02_F", "", 20000},
-         { "30Rnd_9x21_Mag", "", 150},
-         { "SMG_01_F", "", 25000},
-         { "30Rnd_45ACP_MAG_SMG_01", "", 200},
-         { "30Rnd_45ACP_Mag_SMG_01_tracer_green", "", 150},
+        //SMG's
+        { "hgun_PDW2000_F", "", 20000 },//Wenn Legal, im CIV shop anbieten!
+        { "16Rnd_9x21_Mag", "", 100},
+        { "SMG_02_F", "", 20000},
+        { "30Rnd_9x21_Mag", "", 150},
+        { "SMG_01_F", "", 25000},
+        { "30Rnd_45ACP_MAG_SMG_01", "", 200},
+        { "30Rnd_45ACP_Mag_SMG_01_tracer_green", "", 150},
          
 		//Rifles
-         //Underwater Gun
-         { "arifle_SDAR_F", "", 15000},
-         { "20Rnd_556x45_UW_mag", "", 100},
+        //Underwater Gun
+        { "arifle_SDAR_F", "", 15000},
+        { "20Rnd_556x45_UW_mag", "", 100},
          
-         //Assault Rifles
-         { "arifle_Mk20C_plain_F", "", 25000},
-         { "arifle_Mk20_plain_F", "", 30000},
-         { "arifle_Mk20_GL_plain_F", "", 40000},
-         { "arifle_TRG20_F", "", 25000},
-         { "arifle_TRG21_F", "", 30000},
-         { "arifle_TRG21_GL_F", "", 40000},
-         { "30Rnd_556x45_Stanag", "", 200},
-         { "30Rnd_556x45_Stanag_Tracer_Green", "", 150},
-         { "30Rnd_556x45_Stanag_Tracer_Yellow", "", 150},
-         { "30Rnd_556x45_Stanag_Tracer_Red", "", 150},
+        //Assault Rifles
+        { "arifle_Mk20C_plain_F", "", 25000},
+        { "arifle_Mk20_plain_F", "", 30000},
+        { "arifle_Mk20_GL_plain_F", "", 40000},
+        { "arifle_TRG20_F", "", 25000},
+        { "arifle_TRG21_F", "", 30000},
+        { "arifle_TRG21_GL_F", "", 40000},
+        { "30Rnd_556x45_Stanag", "", 200},
+        { "30Rnd_556x45_Stanag_Tracer_Green", "", 150},
+        { "30Rnd_556x45_Stanag_Tracer_Yellow", "", 150},
+        { "30Rnd_556x45_Stanag_Tracer_Red", "", 150},
          
-         { "arifle_Katiba_C_F", "", 25000},
-         { "arifle_Katiba_F", "", 30000 },
-         { "arifle_Katiba_GL_F", "", 40000},
-         { "30Rnd_65x39_caseless_green", "", 200},
-         { "30Rnd_65x39_caseless_green_mag_Tracer", "", 150},
+        { "arifle_Katiba_C_F", "", 25000},
+        { "arifle_Katiba_F", "", 30000 },
+        { "arifle_Katiba_GL_F", "", 40000},
+        { "30Rnd_65x39_caseless_green", "", 200},
+        { "30Rnd_65x39_caseless_green_mag_Tracer", "", 150},
 
-         { "arifle_MXC_F", "", 25000},
-         { "arifle_MXC_Black_F", "", 25000},
-         { "arifle_MX_F", "", 30000},
-         { "arifle_MX_Black_F", "", 35000},
-         { "arifle_MX_GL_F", "", 40000},
-         { "arifle_MX_GL_Black_F", "", 40000},
+        { "arifle_MXC_F", "", 25000},
+        { "arifle_MXC_Black_F", "", 25000},
+        { "arifle_MX_F", "", 30000},
+        { "arifle_MX_Black_F", "", 35000},
+        { "arifle_MX_GL_F", "", 40000},
+        { "arifle_MX_GL_Black_F", "", 40000},
 
-         // Markman Rifles
-         { "arifle_MXM_F", "", 45000},
-         { "arifle_MXM_Black_F", "", 50000},
-         { "30Rnd_65x39_caseless_mag", "", 200},
-         { "30Rnd_65x39_caseless_mag_Tracer", "", 150},
-         { "srifle_DMR_01_F", "", 60000},
-         { "10Rnd_762x54_Mag", "", 150},
-         { "srifle_EBR_F", "", 70000},
+        // Markman Rifles
+        { "arifle_MXM_F", "", 45000},
+        { "arifle_MXM_Black_F", "", 50000},
+        { "30Rnd_65x39_caseless_mag", "", 200},
+        { "30Rnd_65x39_caseless_mag_Tracer", "", 150},
+        { "srifle_DMR_01_F", "", 60000},
+        { "10Rnd_762x54_Mag", "", 150},
+        { "srifle_EBR_F", "", 70000},
 
-         // DLC
-         { "srifle_DMR_06_camo_F", "", 70000},
-         { "srifle_DMR_03_F", "", 75000},
-         { "20Rnd_762x51_Mag", "", 250},
-         { "srifle_DMR_02_F", "", 115000},
-         { "10Rnd_338_Mag", "", 500},
-         { "srifle_DMR_05_blk_F", "", 115000},
-         { "10Rnd_93x64_DMR_05_Mag", "", 500},
+        // DLC
+        { "srifle_DMR_06_camo_F", "", 70000},
+        { "srifle_DMR_03_F", "", 75000},
+        { "20Rnd_762x51_Mag", "", 250},
+        { "srifle_DMR_02_F", "", 115000},
+        { "10Rnd_338_Mag", "", 500},
+        { "srifle_DMR_05_blk_F", "", 115000},
+        { "10Rnd_93x64_DMR_05_Mag", "", 500},
 
-         // Sniper Rifles
-         { "srifle_LRR_LRPS_F", "", 153000},
-         { "srifle_LRR_camo_LRPS_F", "", 184000},
-         { "7Rnd_408_Mag", "", 500},
-         { "srifle_GM6_LRPS_F", "", 191000},
-         { "srifle_GM6_camo_LRPS_F", "", 230000},
-         { "5Rnd_127x108_Mag", "", 500},
-         { "5Rnd_127x108_APDS_Mag", "", 600},
+        // Sniper Rifles
+        { "srifle_LRR_LRPS_F", "", 153000},
+        { "srifle_LRR_camo_LRPS_F", "", 184000},
+        { "7Rnd_408_Mag", "", 500},
+        { "srifle_GM6_LRPS_F", "", 191000},
+        { "srifle_GM6_camo_LRPS_F", "", 230000},
+        { "5Rnd_127x108_Mag", "", 500},
+        { "5Rnd_127x108_APDS_Mag", "", 600},
 
-         { "srifle_DMR_04_F", "", 306000},
-         { "srifle_DMR_04_tan_F", "", 345000},
-         { "10Rnd_127x54_Mag", "", 750}
-
+        { "srifle_DMR_04_F", "", 306000},
+        { "srifle_DMR_04_tan_F", "", 345000},
+        { "10Rnd_127x54_Mag", "", 750},
+		{ "cse_bandage_basic", "Einfache Bandage", 50, 25 }
 		};
 	};
 
@@ -135,7 +136,8 @@ class WeaponShops {
          { "16Rnd_9x21_Mag", "", 25 },
          { "9Rnd_45ACP_Mag", "", 45 },
          { "6Rnd_45ACP_Cylinder", "", 50 },
-         { "30Rnd_9x21_Mag", "", 75 }
+         { "30Rnd_9x21_Mag", "", 75 },
+		 { "cse_bandage_basic", "Einfache Bandage", 50, 25 }
 		};
 	};
 
@@ -151,6 +153,7 @@ class WeaponShops {
 			{ "ToolKit", "", 250 },
 			{ "FirstAidKit", "", 150 },
 			{ "NVGoggles", "", 2000 },
+			{ "cse_bandage_basic", "Einfache Bandage", 50, 25 },
 			{ "Chemlight_red", "", 300 },
 			{ "Chemlight_yellow", "", 300 },
 			{ "Chemlight_green", "", 300 },
@@ -175,6 +178,8 @@ class WeaponShops {
             { "muzzle_snds_L", "", 650, -1 },
             { "FirstAidKit", "", 150, 65 },
             { "Medikit", "", 1000, 450 },
+			{ "cse_morphine", "Autoinjektor: Morphine", 50, 25 },
+			{ "cse_bandage_basic", "Einfache Bandage", 50, 25 },			
             { "NVGoggles", "", 2000, 980 },
             { "16Rnd_9x21_Mag", "", 50, -1 },
             { "20Rnd_556x45_UW_mag", "Taser Rifle Magazine", 125, -1 }
@@ -226,11 +231,38 @@ class WeaponShops {
         level[] = { "", "", -1, "" };
         items[] = {
 			{ "cse_bandage_basic", "Einfache Bandage", 50, 25 },
+			{ "cse_packing_bandage", "Dicker Verband", 60, 25 },
+			{ "cse_bandageElastic", "Elastische Bandage", 70, 40 },
+			{ "cse_tourniquet", "Blutaderstauer", 60, 30 },
+			{ "cse_splint", "Schiene", 70, 40 },
+			{ "cse_morphine", "Autoinjektor: Morphine", 50, 25 },
+			{ "cse_atropine", "Autoinjektor: Atrophine", 50, 25 },
+			{ "cse_epinephrine", "Autoinjektor: Epinephrine", 50, 25 },
+			{ "cse_plasma_iv", "Plasmatransfusion", 100, 50 },
+			{ "cse_plasma_iv_250", "Plasmatransfusion 250ml", 110, 55 },
+			{ "cse_plasma_iv_500", "Plasmatransfusion 500ml", 130, 65 },
+			{ "cse_blood_iv", "Bluttransfusion", 100, 50 },			
+			{ "cse_blood_iv_250", "Bluttransfusion 250ml", 110, 55 },			
+			{ "cse_blood_iv_500", "Bluttransfusion 500ml", 130, 65 },			
+			{ "cse_saline_iv", "Kochsalzlösung", 100, 50 },			
+			{ "cse_saline_iv_250", "Kochsalzlösung 250ml", 110, 55 },
+			{ "cse_saline_iv_500", "Kochsalzlösung 500ml", 130, 65 },
+			{ "cse_quikclot", "Quikclot", 50, 25 },
+			{ "cse_nasopharyngeal_tube", "Nasen-Rachen-Tubus", 25, 12 },
+			{ "cse_opa", "Opa", 50, 25 },
+			{ "cse_liquidSkin", "Sprühpflaster", 5, 1 },
+			{ "cse_chestseal", "Torso VerbandsKorsett", 200, 100 },
+			{ "cse_personal_aid_kit", "Erste Hilfe Kasten", 5, 1 },
+			{ "cse_surgical_kit", "Chirurgen Op-Besteck", 500, 250 },			
+			{ "tf_anprc152", "AN/PRC152 5km", 250, 120 },
+			{ "tf_anprc148jem", "AM/PRC148jem 5km", 250, 120 },
+			{ "tf_fadak", "FADAK 5km", 250, 120 },
+			{ "tf_anarc210", "AN/ARC120 40km", 1000, 500 },
+			{ "tf_anarc164", "AN/ARC164 40km", 1000, 500 },
+			{ "tf_mr6000l", "MR6000L 40km", 1000, 500 },	
             { "ItemGPS", "", 100, 45 },
             { "Binocular", "", 150, -1 },
             { "ToolKit", "", 250, 75 },
-            { "FirstAidKit", "", 150, 65 },
-            { "Medikit", "", 500, 450 },
             { "NVGoggles", "", 1200, 980 },
             { "B_FieldPack_ocamo", "", 3000, -1 }
         };
